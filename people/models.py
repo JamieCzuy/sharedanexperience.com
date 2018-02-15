90ff2a7e28f67d15be813d7c87b379cdbc54d1ac
Jamie <jamie.czuy@gmail.com>
2018-02-14 21:11:28 -0500

    [Add] modelling and migrations for start of people crud

--- people/models.py
+++ people/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Person(models.Model):
+    nickname = models.TextField()
+    full_name = models.TextField()
+
+    def __str__(self):
+        return self.nickname
